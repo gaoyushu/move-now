23eaa448374fa2bc06e05aae91d1414a8dfff345
niuyuxin123 <1120733074@qq.com>
2019-12-04 16:52:24 +0800

    project

---
+++ react/build/precache-manifest.49a5ff4b7407824c83bb6fd87309a8ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479839de3cc204c04f04b78b32f0f387",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1b21d3e37cf78be669c",
+    "url": "/static/css/2.6de58522.chunk.css"
+  },
+  {
+    "revision": "22a509341fbbd05653e9",
+    "url": "/static/css/main.bdcd47fb.chunk.css"
+  },
+  {
+    "revision": "b1b21d3e37cf78be669c",
+    "url": "/static/js/2.dfb5886d.chunk.js"
+  },
+  {
+    "revision": "22a509341fbbd05653e9",
+    "url": "/static/js/main.ebd60c3d.chunk.js"
+  },
+  {
+    "revision": "7d86822d404485c3e247",
+    "url": "/static/js/runtime-main.5df21495.js"
+  }
+]);
