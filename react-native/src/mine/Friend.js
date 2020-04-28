6ec88102ee52cb556ba8058c705be5f461b1db60
zhouhong <q15230189155@163.com>
2020-04-28 12:46:33 +0800

    lalala

--- react-native/src/mine/Friend.js
+++ react-native/src/mine/Friend.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity, Image, ScrollView, AsyncStorage } from 'react-native';
+import { Text, View, TouchableOpacity, Image, ScrollView, AsyncStorage,Dimensions } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient'//渐变插件
 import { Actions } from 'react-native-router-flux';
+
+const { width ,height} = Dimensions.get('window');
 
 export default class Friend extends Component {
     constructor(props) {
@@ -39,32 +41,32 @@ export default class Friend extends Component {
                     start={{ x: 0, y: 0 }}
                     end={{ x: 1, y: 0 }}
                     colors={['#8bcca1', '#57a099']}
-                    style={{ width: '100%', height: 70, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}
+                    style={{ width: width, height: 0.082*height, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}
                 >
                     <TouchableOpacity onPress={Actions.pop}>
                         <Image
-                            style={{ width: 30, height: 30, marginRight: 150, marginLeft: 10 }}
+                            style={{ width: 0.0625*width, height: 0.035*height, marginRight: 0.3125*width, marginLeft: 0.02*width }}
                             source={require('../image/jiantouleft.png')}
                         />
                     </TouchableOpacity>
 
-                    <Text style={{ fontSize: 25, color: 'white' }}>好友列表</Text>
+                    <Text style={{ fontSize: 20, color: 'white' }}>好友列表</Text>
                 </LinearGradient>
                 <ScrollView>
-                    <View style={{ width: 480, flexDirection: 'column', alignItems: 'center', }}>
+                    <View style={{ width: width, flexDirection: 'column', alignItems: 'center', }}>
                         {
                             this.state.status !== -1
                                 ? this.state.data.map((item, key) => (
-                                    <View style={{ marginBottom: 5, width: 480, height: 100, backgroundColor: 'white', flexDirection: 'row', justifyContent: "center", alignItems: "center" }}>
-                                        <View style={{ width: 400, height: 80, flexDirection: "row", alignItems: 'center' }}>
+                                    <View style={{ marginBottom: 0.006*height, width:width, height: 0.001*height, backgroundColor: 'white', flexDirection: 'row', justifyContent: "center", alignItems: "center" }}>
+                                        <View style={{ width: 0.833*width, height: 0.094*height, flexDirection: "row", alignItems: 'center' }}>
                                             <TouchableOpacity onPress={()=>Actions.person({uid:item.uid})}>
                                                 <Image
-                                                    style={{ width: 75, height: 75, borderRadius: 37.5 }}
+                                                    style={{ width: 0.1562*width, height: 0.088*height, borderRadius: 0.088*height }}
                                                     source={{ uri: 'http://116.62.14.0:8666/api/image/' + this.state.data[key].uimage }} />
                                             </TouchableOpacity>
-                                            <View style={{ marginLeft: 20, width: 305, height: 80 }}>
-                                                <Text style={{ fontSize: 18, marginBottom: 10 }}>{item.uname}</Text>
-                                                <Text style={{ fontSize: 18, color: 'gray' }}>{item.uintroduce}</Text>
+                                            <View style={{ marginLeft: 0.042*width, width: 0.635*width, height: 0.094*height}}>
+                                                <Text style={{ fontSize: 13, marginBottom: 0.012*height }}>{item.uname}</Text>
+                                                <Text style={{ fontSize: 13, color: 'gray' }}>{item.uintroduce}</Text>
                                             </View>
                                         </View>
                                     </View>
